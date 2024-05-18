86b1ed27106e22d34f9c56c6d3357f0627069e9e
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-17 21:29:38 -0300

    primeira aula 17/05

--- src/main/java/com/github/tiagoalcan/mvc_rh/controller/DepartamentoController.java
+++ src/main/java/com/github/tiagoalcan/mvc_rh/controller/DepartamentoController.java
@@ -1,25 +1,58 @@
 package com.github.tiagoalcan.mvc_rh.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.github.tiagoalcan.mvc_rh.model.Departamento;
+import com.github.tiagoalcan.mvc_rh.repository.DepartamentoRepository;
 
 @Controller
 @RequestMapping("departamentos")
 public class DepartamentoController {
 
+	@Autowired
+	private DepartamentoRepository departamentoRepository;
+	
 	@GetMapping()
-	public String list() {
+	public String list(Model model) {
+		List<Departamento> departamentos = departamentoRepository.findAll();
+		model.addAttribute("departamentos", departamentos);	
 		return "departamento/list";
 	}
 	
-	@GetMapping("cargos")
-	public String cargosByDepartamento() {
-		return "departamento/list_cargo";
+	@PostMapping("save")
+	public String save(@ModelAttribute Departamento departamento) {
+		departamentoRepository.save(departamento);
+		return "redirect:/departamentos";
 	}
 	
-	@GetMapping("form")
-	public String form() {
+	@GetMapping("add")
+	public String create(Model model) {
+		model.addAttribute("departamento", new Departamento());
 		return "departamento/form";
+	}
+	
+	@GetMapping("update/{id}")
+	public String update(@PathVariable Long id, Model model) {
+
+		Departamento departamento = departamentoRepository.findById(id).orElse(new Departamento());
+
+		model.addAttribute("departamento", departamento);
+		return "departamento/form";
+	}
+	
+	@GetMapping("delete/{id}")
+	public String delete(@PathVariable Long id) {
+		departamentoRepository.deleteById(id);
+		
+		return "redirect:/departamentos";
 	}
 }
---
+++ src/main/resources/scripts/data-dev.sql
@@ -0,0 +1,11 @@
+insert into funcionarios (id, nome_funcionario, data_entrada) values (nextval('funcionarios_seq'), 'Pelé', '1950-12-31');
+
+insert into funcionarios (id, nome_funcionario, data_entrada) values (nextval('funcionarios_seq'), 'Maradona', '1990-01-20');
+
+insert into funcionarios (id, nome_funcionario, data_entrada) values (nextval('funcionarios_seq'), 'Tiago', '2003-06-13');
+
+insert into departamentos (id, nome) values (nextval('departamentos_seq'), 'RH');
+
+insert into departamentos (id, nome) values (nextval('departamentos_seq'), 'Tecnologia de informação');
+
+insert into departamentos (id, nome) values (nextval('departamentos_seq'), 'Financeiro');
